f8a76559a21d2611e67161d4b09f5b5b7c124449
Nathan LeSueur <nathanle@akamai.com>
2025-08-25 17:18:44 -0400

    Add changes from configmap to test script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-grep "LimitNOFILE=infinity" /usr/lib/systemd/system/containerd.service 
+grep "LimitNOFILE=infinity" /usr/lib/systemd/system/containerd.service
 if [ $? -ne 0 ]; then
-  sed -i '/^LimitCORE/a\LimitNOFILE=infinity' /usr/lib/systemd/system/containerd.service && systemctl daemon-reload && systemctl restart containerd
+  sed -i '/^LimitCORE/a\LimitNOFILE=infinity\nLimitMEMLOCK=8M' /usr/lib/systemd/system/containerd.service && systemctl daemon-reload && systemctl restart containerd
   echo "Patching $HOSTNAME"
 fi
